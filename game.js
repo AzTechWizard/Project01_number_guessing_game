aadb7a68b9d0d9517a31c15234994db7f37f2900
AzTechWizard <160797377+AzTechWizard@users.noreply.github.com>
2024-03-25 01:07:11 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const random = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "Guess any number (1-10):",
+        type: "number",
+        name: "guess"
+    },
+]);
+if (answer.guess === random) {
+    console.log(`Congratulations...You Wonn!!!!`);
+}
+else {
+    console.log(`You Loose! Try Again!`);
+}
